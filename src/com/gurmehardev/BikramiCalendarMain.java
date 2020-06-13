a60c1b1beb6bda80053f2071ddda0396c17f8a4f
Gurmehar <50474292+thedovesscar@users.noreply.github.com>
2020-06-12 21:00:15 -0700

    Setting up for Initial Commit of Project
    setting up the normal req for Java Project on Eclipse. working on PC. created Driver class and The main JFrame class.

---
+++ src/com/gurmehardev/BikramiCalendarMain.java
@@ -0,0 +1,18 @@
+package com.gurmehardev;
+
+import com.gurmehardev.frameworks.MyFrame;
+
+/**
+ * The basic Driver main() class to get this thing rolling! 
+ * 
+ * @author Gurmehar Singh
+ *
+ */
+public class BikramiCalendarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MyFrame();
+	}
+
+}
---
+++ src/com/gurmehardev/frameworks/MyFrame.java
@@ -0,0 +1,34 @@
+package com.gurmehardev.frameworks;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * this is the custom JFrame class that holds the entire
+ * program 
+ * 
+ * @author Gurmehar Singh
+ *
+ */
+public class MyFrame extends JFrame {
+
+	/**
+	 * generated serialVersionUID
+	 */
+	private static final long serialVersionUID = -920798717489805177L;
+
+	public MyFrame() throws HeadlessException {
+
+		super("Bikrami Calendar Conversion");
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(1405, 1005);
+		setResizable(false);
+		
+		setVisible(true);
+	}
+
+	
+
+}
